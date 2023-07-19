e0c069e2090b0f290587da09f6c7dae979c05639
Camelia03 <>
2023-07-19 10:52:36 +0000

    Set up environment variables and automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,13 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8g&8f#fiqzx@pp1%xkl&+@uq5(6v#20&#c)z(+3r2j#^9@4$zg'
+SECRET_KEY = os.environ.get(
+    'SECRET_KEY', 'django-insecure-8g&8f#fiqzx@pp1%xkl&+@uq5(6v#20&#c)z(+3r2j#^9@4$zg')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-camelia03-fsf-hello-djan-sectaifvou.us2.codeanyapp.com',
-                 'camelia-django-todo-app-98fd4d26f019.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
